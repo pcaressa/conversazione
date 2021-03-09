81760719cd13aeaca52b48a04de09f5c865d6601
Paolo Caressa <pcaressa@users.noreply.github.com>
2021-03-09 22:57:20 +0100

    Add files via upload

---
+++ conversazione.py
@@ -0,0 +1,33 @@
+import pickle
+import markov
+
+NOME = "Gramsci"
+NOME_FILE = "gramsci.txt"
+ENC = "latin-1"
+#NOME = "Dante"
+#NOME_FILE = "divinacommedia.txt"
+#ENC = "latin-1"
+#NOME_FILE = "pensieri.txt"
+#NOME = "Leopardi"
+#ENC = "latin-1"
+
+# Se il file NOME.pkl esiste lo carica, se no lo crea e lo salva
+try:
+    with open(NOME_FILE + ".pkl", "rb") as f:
+        dixit = pickle.load(f)
+except:
+    markov.analizza(NOME_FILE, enc=ENC)
+    dixit = {}
+    markov.dizionario(dixit, NOME_FILE + ".tok", 5)
+    with open(NOME_FILE + '.pkl', 'wb') as f:
+        pickle.dump(dixit, f, pickle.HIGHEST_PROTOCOL)
+
+print("Conversazione con un pazzo che si crede", NOME)
+while True:
+    domanda = input("Tu - ")
+    if domanda == "":
+        break
+    if domanda[-1] in [".", "?", "!", ";", ","]:
+        domanda = domanda[:-1]
+    risposta = markov.genera(dixit, domanda)
+    print(NOME, "-", risposta)
